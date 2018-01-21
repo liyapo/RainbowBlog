262b52f2024dee3c783b78771bd264e4544c78dc
liyapo <liyapo@mail.ru>
2018-01-21 18:46:09 +0100

    Added model for Article, updated  base.html and listArticles.html, urls.pu in articles and rainbow files, updated views for listArticles, and articles registered in admin site

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Articles
 
 # Create your views here.
+def list_articles(request):
+    list_articles = Articles.objects.all()
+    context = {'articles': list_articles}
+    return render(request, 'articles/listArticles.html', context)
+
